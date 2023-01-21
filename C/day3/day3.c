caee73cdca604b67c38c33281219ec8ea289eea0
Bartosz Lenart <lenart.consulting@gmail.com>
2023-01-21 12:17:42 +0100

    day3 task 2

--- C/day3/day3.c
+++ C/day3/day3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 const int CHUNK_SIZE = 256;
 
@@ -32,6 +33,55 @@ char find_diff_char(char *a, int last)
     }
 
     return '0';
+}
+
+char *get_repetitions(char *str_a,char *str_b)
+{
+    int i = 0; 
+    unsigned char bits[32];
+    
+    for(int i = 0; i < 32; i++ )
+    {    
+        bits[i] = 0;
+    }
+
+    while(true)
+    {
+        char c = str_a[i];
+        if (c == '\n')
+        {
+            break;
+        }
+        int j = c/8;
+        unsigned char mask = 1<<(c%8);
+        bits[j] |= mask;
+        i++;
+    }
+
+    
+    i = 0;
+    int next = 0;
+    char *repetions = calloc(CHUNK_SIZE, sizeof(char));
+    while(true)
+    {
+        char c = str_b[i];
+        if (c == '\n')
+        {
+            break;
+        }
+        int j = c/8;
+        unsigned char mask = 1<<(c%8);
+        if ((bits[j]&mask) != 0)
+        {
+            repetions[next] = c; 
+            next++;
+        }
+        i++;
+    }
+
+    repetions[next+1] = '\n';
+    return repetions;
+
 }
 
 int read_priority(char c)
@@ -76,13 +126,50 @@ void solve_1(const char *file_name)
     printf("solution to task 3.1 for data from %s is: %d\n", file_name, score);
 }
 
+void solve_2(const char *file_name)
+{
+
+    FILE * fp;
+
+    fp = fopen(file_name, "r");
+    if (fp == NULL) exit(EXIT_FAILURE);
+    char chunk[CHUNK_SIZE];
+    int score = 0;
+    int counter = 0;
+    char group[3][CHUNK_SIZE];
+    while(fgets(chunk, sizeof(chunk), fp) != NULL)
+    {
+        if (strcmp(chunk, "\n") == 0)
+        {
+            continue;
+        }
+
+        memcpy(group[counter], chunk, sizeof(group[counter]));
+
+        if (counter != 0 && counter %2 == 0)
+        {
+            char *repetions = get_repetitions(group[0], group[1]);
+            char *final_rep = get_repetitions(repetions, group[2]);
+            score += read_priority(final_rep[0]);
+            counter = 0;
+            continue;
+        }
+        counter++;
+    }
+
+    printf("solution to task 3.2 for data from %s is: %d\n", file_name, score);
+}
+
 
 int main()
 {
     char const *FILE_TEST = "./data_test.txt"; 
     solve_1(FILE_TEST);
+    solve_2(FILE_TEST);
 
     char const *FILE_TASK = "./data_task.txt"; 
     solve_1(FILE_TASK);
+    solve_2(FILE_TASK);
+    
     return 0;
 }
